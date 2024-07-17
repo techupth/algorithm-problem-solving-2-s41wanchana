9b86674a4228cacbfafa9752a7bb12498310f80e
Wanchana <s.wanchana41@gmail.com>
2024-07-17 14:30:35 +0700

    feat:add bianry search in ex-1 and ex-2

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,31 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let left = 0, right = students.length - 1; left <= right; ) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (students[mid] === searchStudent) {
+      return mid;
+    } else if (students[mid] < searchStudent) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
 }
 
+let students = [
+  "Andrew",
+  "Bobby",
+  "Diana",
+  "George",
+  "Hannah",
+  "Isaac",
+  "Jasmine",
+  "John",
+];
+findStudentIndex(students, "John");
+
 // ตอบคำถามตรงนี้จ้า
+// O(log n) เพราะในทุกๆรอบการประมวลผล binary search จะทำการลดข้อมูลที่ใช้ลงครึ่งนึงของรอบก่อนหน้า
